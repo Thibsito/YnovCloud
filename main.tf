c2501efc5895f8e76aeabb64cb0d2c2e8df60e52
Thibsito <thibault.cimino@gmail.com>
2023-07-04 14:23:02 +0000

    v0.3

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-resource "aws_ssm_parameter" "labite" { # changer bobby
-  name  = "labite" # changer bobby
+resource "aws_ssm_parameter" "thibs" { # changer bobby
+  name  = "thibs" # changer bobby
   type  = "String"
   value = "kevin est vraiment nice" # changer ça
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
